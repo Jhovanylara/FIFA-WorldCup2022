7e8b3327f51679c90ad8ae82c4a10ba6248853cf
Jhovanylara <jhovanylara@hotmail.com>
2022-11-22 13:35:08 -0600

    Jhovany3

--- WorldCup.py
+++ WorldCup.py
@@ -33,7 +33,7 @@ elif Grupo=='GroupH':
 Team=st.selectbox("Select a Team:", (Grupo))
 
 def getlocal(FIFATeam):
-    R1=f'''SELECT Year, Stage, `Home Team Name` AS `home_team`, `Away Team Name` AS `away_team`, `Home Team Goals` AS `home_goals`, `Away Team Goals` AS `away_goals`, `Half-time Home Goals`, `Half-time Away Goals`, `Home result` AS `home_result`, `MatchID`
+    R1=f'''SELECT Year, Stage, `Home Team Name` AS `home_team`, `Away Team Name` AS `away_team`, `Home Team Goals` AS `home_score`, `Away Team Goals` AS `away_score`, `Half-time Home Goals`, `Half-time Away Goals`, `Home result` AS `home_result`, `MatchID`
     FROM matches
     WHERE `Home Team Name` = '{FIFATeam}'
     GROUP BY `MatchID`
@@ -43,9 +43,18 @@ def getlocal(FIFATeam):
     
     return df
 
+def wcgetlocal(FIFATeam):
+    R1=f'''SELECT `year`, `stage`, `home_team`, `away_team`, `home_score`, `away_score`, `outcome`, `winning_team`
+    FROM wcmatches
+    WHERE `home_team` = '{FIFATeam}'
+    ;'''
+
+    df=pd.read_sql(R1, engine)
+    
+    return df
 
 def getvisit(FIFATeam):
-    R1=f'''SELECT Year, Stage, `Home Team Name` AS `home_team`, `Away Team Name` AS `away_team`, `Home Team Goals` AS `home_goals`, `Away Team Goals`  AS `away_goals`, `Half-time Home Goals`, `Half-time Away Goals`, `Home result` AS `home_result`, `MatchID`
+    R1=f'''SELECT Year, Stage, `Home Team Name` AS `home_team`, `Away Team Name` AS `away_team`, `Home Team Goals` AS `home_score`, `Away Team Goals`  AS `away_score`, `Half-time Home Goals`, `Half-time Away Goals`, `Home result` AS `home_result`, `MatchID`
     FROM matches
     WHERE `Away Team Name` = '{FIFATeam}'
     GROUP BY `MatchID`
@@ -54,50 +63,74 @@ def getvisit(FIFATeam):
     df=pd.read_sql(R1, engine)
     return df
 
+def wcgetvisit(FIFATeam):
+    R1=f'''SELECT `year`, `stage`, `home_team`, `away_team`, `home_score`, `away_score`, `outcome`, `winning_team`
+    FROM wcmatches
+    WHERE `away_team` = '{FIFATeam}'
+    ;'''
+
+    df=pd.read_sql(R1, engine)
+    
+    return df
+
+    
+
 df1=getlocal(Team)
-df2=getvisit(Team) #FIFATeam=Team
+df2=getvisit(Team)
+df3=wcgetlocal(Team)
+df4=wcgetlocal(Team)
 
 
 tab1, tab2, tab3= st.tabs(['Local',"Visitor","Global"])
 with tab1:
     '''### As local:'''
-    if df1.empty:
+    if df3.empty:
         f"This is the first appearance of {Team} in the World Cup."
     else:
-        G1=df1['home_goals'].sum()
-        G2=df1['away_goals'].sum()
+        G1=df3['home_score'].sum()
+        G2=df3['away_score'].sum()
+        G1_1=df1['home_score'].sum()
+        G2_1=df1['away_score'].sum()
         G3=df1['Half-time Home Goals'].sum()
         G4=df1['Half-time Away Goals'].sum()
-        partidos=len(df1.axes[0])
-        wins_local=df1.home_result.value_counts().Won
-        lost_local=df1.home_result.value_counts().Lost
-        tied_local=df1.home_result.value_counts().Tied
+        partidos=len(df3.axes[0])
+        wins_local=df3.outcome.value_counts().H
+        lost_local=df3.outcome.value_counts().A
+        if 'D' in df3['outcome'].unique():
+            tied_local=df3.outcome.value_counts().D
+        else:
+            tied_local=0
 
         f"{Team} has scored {G1} goals and conceded {G3} goals, in {partidos} matches."
         f"In the first half has scored {G3} goals and conceded {G4} goals."
-        f"The {round((1-G3/G1)*100,1)}% of their goals are scored in the second half."
-        f"The {round((1-G4/G2)*100,1)}% of conceded goalas are scored in the second half."
+        f"The {round((1-G3/G1_1)*100,1)}% of their goals are scored in the second half."
+        f"The {round((1-G4/G2_1)*100,1)}% of conceded goalas are scored in the second half."
         f"Has won as local {wins_local} matches, has lost {lost_local} matches and has tied {tied_local} matches. With a {round((wins_local/partidos)*100,1)}% win rate."
 
         df1=df1.sort_values(by=['Year'])
         
 with tab2:
     '''### As a visitor:'''
-    if df2.empty:
+    if df3.empty:
         f"This is the first appearance of {Team} in the World Cup."
     else:
-        G5=df2['home_goals'].sum()
-        G6=df2['away_goals'].sum()
+        G5=df4['home_score'].sum()
+        G6=df4['away_score'].sum()
+        G5_1=df2['home_score'].sum()
+        G6_1=df2['away_score'].sum()
         G7=df2['Half-time Home Goals'].sum()
         G8=df2['Half-time Away Goals'].sum()
         partidos_visit=len(df2.axes[0])
-        wins_visit=df2.home_result.value_counts().Lost #Se pone como Lost porque al ser visitante, si ha perido el local, ha ganado el visitante.
-        lost_visit=df2.home_result.value_counts().Won
-        tied_visit=df2.home_result.value_counts().Tied
+        wins_visit=df4.outcome.value_counts().A #Se pone como Lost porque al ser visitante, si ha perido el local, ha ganado el visitante.
+        lost_visit=df4.outcome.value_counts().H
+        if 'D' in df4['outcome'].unique():
+            tied_visit=df4.outcome.value_counts().D
+        else:
+            tied_visit=0
         f"{Team} has scored {G6} goals and conceded {G5} goals, in {partidos_visit} matches."
         f"In the first half has scored {G8} goals and conceded {G7} goals."
-        f"The {round((1-G8/G6)*100,1)}% of their goals are scored in the second half."
-        f"The {round((1-G7/G5)*100,1)}% of conceded goalas are scored in the second half."
+        f"The {round((1-G8/G6_1)*100,1)}% of their goals are scored in the second half."
+        f"The {round((1-G7/G5_1)*100,1)}% of conceded goalas are scored in the second half."
         f"Has won as local {wins_visit} matches, has lost {lost_visit} and has tied {tied_visit} matches. With a {round((wins_visit/partidos_visit)*100,1)}% win rate."
 
 with tab3:
@@ -106,7 +139,9 @@ with tab3:
         f"This is the first appearance of {Team} in the World Cup."
     else:
         Anotados=G1+G6
+        Anotados_2014=G1_1+G6_1
         Recibidos=G2+G5
+        Recibidos_2014=G2_1+G5_1
         Anotados1T=G3+G8
         Recibidos1T=G4+G7
         Partidos_totales=partidos+partidos_visit
@@ -116,8 +151,8 @@ with tab3:
 
         f"{Team} has scored {Anotados} goals and conceded {Recibidos} goals, in {Partidos_totales} matches."
         f"In the first half has scored  {Anotados1T} goals and conceded {Recibidos1T} goals"
-        f"The {round((1-Anotados1T/Anotados)*100,1)}% of their goals are scored in the second half ({Anotados-Anotados1T})."
-        f"The {round((1-Recibidos1T/Recibidos)*100,1)}% of conceded goals are scored in the second half."
+        f"The {round((1-Anotados1T/Anotados_2014)*100,1)}% of their goals are scored in the second half."
+        f"The {round((1-Recibidos1T/Recibidos_2014)*100,1)}% of conceded goals are scored in the second half."
         f"{Team} has won {wins} matches, has lost {lost} matches and has tied {tied}  matches. With a {round((wins/Partidos_totales)*100,1)}% win rate."
 
         lista1=df1['away_team'].unique()
@@ -149,9 +184,14 @@ with tab3:
                 showlegend = False
                 )
 
-        layout = go.Layout(                                    
+        layout = go.Layout(       
+                        title={'text': 'Top 20 rivals faced',
+                                'y':0.9,
+                                'x':0.5,
+                                'xanchor': 'center',
+                                'yanchor': 'top' },                            
                         xaxis_title='Rivals',
-                        yaxis_title='Matches'
+                        yaxis_title='# of matches'
                         )
         data = [trace]
         fig = go.Figure(data=data,layout = layout)
--- config/db.py
+++ config/db.py
@@ -2,14 +2,19 @@ from sqlalchemy import create_engine, MetaData
 import pandas as pd
 
 matchesURL='https://raw.githubusercontent.com/Jhovanylara/FIFA-WorldCup2022/master/config/WorldCupMatches.csv'
-matches2URL=
+wcmatchesURL='https://raw.githubusercontent.com/Jhovanylara/FIFA-WorldCup2022/master/Datasets/wcmatches.csv'
 cupsURL='https://raw.githubusercontent.com/Jhovanylara/FIFA-WorldCup2022/master/config/WorldCups.csv'
+
 matches=pd.read_csv(matchesURL)
 cups=pd.read_csv(cupsURL)
-matches2=pd.read(matches2URL)
+wcmatches=pd.read_csv(wcmatchesURL)
+
 matches=matches.dropna(how='all')
 matches.drop(columns=['Win conditions','Attendance','City','Stadium','Referee','Assistant 1', 'Assistant 2', 'Home Team Initials','Away Team Initials'], inplace=True)
 matches=matches.replace({'German DR':'Germany','Germany FR':'Germany'})
+
+wcmatches.drop(columns=['country','city','win_conditions','month', 'dayofweek', 'losing_team'], inplace=True)
+wcmatches['winning_team']=wcmatches['winning_team'].fillna('Tied')
 
 #Creamos una nueva columna con el resultado del local, (Win, tie o Lost)
 def wins(row):
@@ -28,6 +33,7 @@ SQLALCHEMY_DATABASE_URL = "mysql+pymysql://root:Uwovuqo_86@localhost/FIFA"
 engine = create_engine(SQLALCHEMY_DATABASE_URL)
 with engine.connect() as conn, conn.begin(): 
     matches.to_sql("matches", conn, if_exists='append', index=False)
+    wcmatches.to_sql("wcmatches", conn, if_exists='append', index=False)
     cups.to_sql("cups", conn, if_exists='append', index=False)
     
 meta= MetaData()
